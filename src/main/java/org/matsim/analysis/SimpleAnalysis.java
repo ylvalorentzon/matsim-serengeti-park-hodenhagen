64da3ed4cc4bcf20d0534a45eb85de3bdbff32bf
ylvalorentzon <ylvalorentzon@ylvas-mbp.localdomain>
2024-11-15 12:01:25 -0800

    My first simple event handler. Using 10 iterations instead of 100.

---
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -0,0 +1,16 @@
+package org.matsim.analysis;
+
+import org.matsim.core.events.EventsUtils;
+
+public class SimpleAnalysis {
+
+	public static void main(String[] args ) {
+		
+		var handler = new SimplePersonEventHandler();
+		var manager = EventsUtils.createEventsManager();
+		manager.addHandler(handler);
+		EventsUtils.readEvents(manager,"/Users/ylvalorentzon/git/matsim-serengeti-park-hodenhagen/scenarios/serengeti-park-v1.0/input/scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml");
+		
+		
+	}
+}
